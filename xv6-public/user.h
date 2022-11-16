f94f11e1e3c92c597b07b51a74317a3ddfa564d4
Caroline Taft <taft@royal-19.cs.wisc.edu>
2022-11-16 17:43:51 -0600

    added ulib declarations and user.h declarations

--- xv6-public/user.h
+++ xv6-public/user.h
@@ -1,5 +1,8 @@
 struct stat;
 struct rtcdate;
+struct lock_t {
+    int flag;
+};
 
 // system calls
 int fork(void);
@@ -39,3 +42,8 @@ void* memset(void*, int, uint);
 void* malloc(uint);
 void free(void*);
 int atoi(const char*);
+int thread_create(void (*start_routine)(void *, void *), void *arg1, void *arg2);
+int thread_join();
+void lock_acquire(lock_t *);
+void lock_release(lock_t *);
+void lock_init(lock_t *);
